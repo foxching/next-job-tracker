7fbd0ed796e953fb476415eed417ecaadae57ce0
Rechie Lagria <rechielagria@gmail.com>
2026-04-18 14:13:00 +0800

    add pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,9 +14,11 @@ export default function Home() {
               Capture, organize and manage all your job applications in one place.
             </p>
             <div className="flex flex-col items-center gap-4">
-              <Button size="lg" className="h-12 px-8 text-lg font-medium">
-                Start for free<ArrowRight className="ml-2" />
-              </Button>
+              <Link href="/sign-up" className="text-primary">
+                <Button size="lg" className="h-12 px-8 text-lg font-medium ">
+                  Start for free<ArrowRight className="ml-2" />
+                </Button>
+              </Link>
               <p className="text-sm text-muted-foreground "> Free forever. No credit card required. No spam. We promise.</p>
             </div>
           </div>
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,6 @@
+const SignIn = () => {
+    return (
+        <h1>Signin</h1>
+    )
+}
+export default SignIn
