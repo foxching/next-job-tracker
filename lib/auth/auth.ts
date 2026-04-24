15cdb5fa222cca1e350f4f7827d34392a8481022
Rechie Lagria <rechielagria@gmail.com>
2026-04-24 23:10:21 +0800

    full login/logout implementation

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,11 +1,31 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { headers } from "next/headers";
 import { getDb } from "@/lib/db";
+import { redirect } from "next/navigation";
 
 export const auth = betterAuth({
     database: mongodbAdapter(await getDb()),
     emailAndPassword: {
         enabled: true,
     },
-    // ...rest of config
+
 });
+
+export async function getSession() {
+    const result = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    return result;
+}
+
+export async function signOut() {
+    const result = await auth.api.signOut({
+        headers: await headers(),
+    });
+
+    if (result.success) {
+        redirect("/sign-in");
+    }
+}
