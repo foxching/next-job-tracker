6c5e7241a56ad4c7b036397f7aa93f33dbe93214
Rechie Lagria <rechielagria@gmail.com>
2026-05-10 17:16:23 +0800

    drag and drop functionality

--- components/job-application-card.tsx
+++ components/job-application-card.tsx
@@ -15,9 +15,10 @@ import { Input } from "./ui/input";
 interface JobApplicationCardProps {
     job: JobApplication;
     columns: Column[];
+    dragHandleProps?: React.HTMLAttributes<HTMLElement>;
 }
 
-export default function JobApplicationCard({ job, columns }: JobApplicationCardProps) {
+export default function JobApplicationCard({ job, columns, dragHandleProps, }: JobApplicationCardProps) {
     const [isEditing, setIsEditing] = useState(false);
     const [formData, setFormData] = useState({
         company: job.company,
@@ -72,7 +73,7 @@ export default function JobApplicationCard({ job, columns }: JobApplicationCardP
     }
     return (
         <>
-            <Card className="cursor-pointer transition-shadow hover:shadow-lg bg-white group shadow-sm">
+            <Card className="cursor-pointer transition-shadow hover:shadow-lg bg-white group shadow-sm"  {...dragHandleProps}>
                 <CardContent className="p-4">
                     <div className="flex items-start justify-between gap-2">
                         <div className="flex-1 min-w-0">
