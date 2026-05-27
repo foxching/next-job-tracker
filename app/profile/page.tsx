dda40effb5c92d5bd7968965f5995ef2cb1d285b
Rechie Lagria <rechielagria@gmail.com>
2026-05-27 09:06:26 +0800

    fix deployement issues

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,16 +1,23 @@
 import ProfileContent from "@/components/profile-content";
 import { getSession } from "@/lib/auth/auth";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
-
-export default async function ProfilePage() {
+async function ProfileData() {
     const session = await getSession();
 
     if (!session) {
         redirect("/sign-in");
     }
 
+    return <ProfileContent session={session.user} />;
+}
+
+export default function ProfilePage() {
     return (
-        <ProfileContent session={session.user} />
+        <Suspense fallback={<div>Loading...</div>}>
+            <ProfileData />
+        </Suspense>
     );
 }
+
