a87baaf05383cf0e478827d941884ed6013a3cb3
Rechie Lagria <rechielagria@gmail.com>
2026-07-04 14:21:12 +0800

    fix dashboard skeleton

--- components/skeleton/DashboardSkeleton.tsx
+++ components/skeleton/DashboardSkeleton.tsx
@@ -3,13 +3,22 @@ import { Skeleton } from "@/components/ui/skeleton";
 export const DashboardSkeleton = () => {
     return (
         <div className="min-h-screen bg-background text-foreground p-6">
-            <div className="container mx-auto space-y-6">
+            <div className="w-full px-2 py-2">
 
-                <Skeleton className="h-8 w-64" />
-                <Skeleton className="h-4 w-64" />
+                <div className="flex items-center justify-between mb-6">
+                    <div className="flex gap-2 py-2">
+                        <Skeleton className="h-8 w-44 " />
+                        <Skeleton className="h-8 w-44" />
+                    </div>
+                    <div className="flex gap-2 py-2">
+                        <Skeleton className="h-8 w-44" />
+                        <Skeleton className="h-8 w-44" />
+                        <Skeleton className="h-8 w-44" />
+                    </div>
+                </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
-                    {Array.from({ length: 4 }).map((_, index) => (
+                <div className="grid grid-cols-1 md:grid-cols-5 gap-2">
+                    {Array.from({ length: 5 }).map((_, index) => (
                         <Skeleton key={index} className="h-120 rounded-lg" />
                     ))}
                 </div>
